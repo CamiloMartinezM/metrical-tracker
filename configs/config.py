ea8319a8006f4c7f717354db5320ae49365a2c56
Camilo Martinez <cama00005@stud.uni-saarland.de>
2025-07-08 17:29:29 +0200

    Missing save_folder update to include subdir from config.py

--- configs/config.py
+++ configs/config.py
@@ -14,23 +14,23 @@
 #
 # Contact: mica@tue.mpg.de
 
-import yaml 
+import yaml
 import argparse
 from pathlib import Path
 from yacs.config import CfgNode as CN
 
 cfg = CN()
 
-cfg.flame_geom_path = 'data/FLAME2020/generic_model.pkl'
-cfg.flame_template_path = 'data/uv_template.obj'
-cfg.flame_lmk_path = 'data/landmark_embedding.npy'
-cfg.tex_space_path = 'data/FLAME2020/FLAME_texture.npz'
+cfg.flame_geom_path = "data/FLAME2020/generic_model.pkl"
+cfg.flame_template_path = "data/uv_template.obj"
+cfg.flame_lmk_path = "data/landmark_embedding.npy"
+cfg.tex_space_path = "data/FLAME2020/FLAME_texture.npz"
 
 cfg.num_shape_params = 300
 cfg.num_exp_params = 100
 cfg.tex_params = 140
-cfg.actor = ''
-cfg.config_name = ''
+cfg.actor = ""
+cfg.config_name = ""
 cfg.kernel_size = 7
 cfg.sigma = 9.0
 cfg.keyframes = [0]
@@ -42,11 +42,16 @@ cfg.image_size = [512, 512]  # height, width
 cfg.rotation_lr = 0.2
 cfg.translation_lr = 0.003
 cfg.raster_update = 8
-cfg.pyr_levels = [[1.0, 160], [0.25, 40], [0.5, 40], [1.0, 70]]  # Gaussian pyramid levels (scaling, iters per level) first level is only the sparse term!
+cfg.pyr_levels = [
+    [1.0, 160],
+    [0.25, 40],
+    [0.5, 40],
+    [1.0, 70],
+]  # Gaussian pyramid levels (scaling, iters per level) first level is only the sparse term!
 cfg.optimize_shape = False
 cfg.optimize_jaw = False
 cfg.crop_image = True
-cfg.save_folder = './output/'
+cfg.save_folder = "./output/"
 
 # Weights
 cfg.w_pho = 350
@@ -63,8 +68,8 @@ cfg.w_tex = 0.04
 cfg.w_jaw = 0.05
 
 # Additional configuration keys added to allow a base configuration
-cfg.is_base_config = None  
-cfg.test_run = None  
+cfg.is_base_config = None
+cfg.test_run = None
 
 
 def get_cfg_defaults():
@@ -78,19 +83,19 @@ def update_cfg(cfg, cfg_file):
 
 def load_yaml_file(file_path: str) -> dict:
     """Load a YAML file and return its contents as a dictionary.
-    
+
     Args:
         file_path (str): The path to the YAML file to be loaded.
-    
+
     Returns:
         dict: A dictionary containing the contents of the YAML file.
-    
+
     Raises:
         FileNotFoundError: If the specified file does not exist.
         yaml.YAMLError: If there's an error parsing the YAML file.
     """
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             return yaml.safe_load(file)
     except FileNotFoundError:
         print(f"Error: File not found at {file_path}")
@@ -102,10 +107,10 @@ def load_yaml_file(file_path: str) -> dict:
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', type=str, help='Configuration file', required=True)
+    parser.add_argument("--cfg", type=str, help="Configuration file", required=True)
 
     args = parser.parse_args()
-    print(args, end='\n\n')
+    print(args, end="\n\n")
 
     cfg = get_cfg_defaults()
     if args.cfg is not None:
@@ -128,7 +133,7 @@ def parse_cfg(cfg_file):
 
 def generate_configs_from_base(base_cfg: dict) -> list[dict]:
     """Generate tracker configurations for each video inside the base actor folder.
-    
+
     The `base_cfg` could look like this:
 
     ```
@@ -136,15 +141,15 @@ def generate_configs_from_base(base_cfg: dict) -> list[dict]:
     save_folder: './bulk/data/interim/face_recordings/Metrical-Tracker/'
     ...
     ```
-    And this file will be used to create the tracker config for each video of the dataset. E.g., 
-    one video is in: `/bulk/data/processed/face_recordings/2/bite_lower_lip/video.mp4`, but the 
+    And this file will be used to create the tracker config for each video of the dataset. E.g.,
+    one video is in: `/bulk/data/processed/face_recordings/2/bite_lower_lip/video.mp4`, but the
     original config file has to have the following path in actor key:
     `actor: './bulk/data/processed/face_recordings/2/bite_lower_lip/'`
     But there are many subfolders inside `face_recordings/2` and also in `face_recordings/3`, etc.
 
     Args:
         base_cfg (dict): The base configuration dictionary containing the actor path.
-    
+
     Returns:
         list[dict]: A list of configuration dictionaries for each video found in the actor path.
     """
@@ -158,9 +163,11 @@ def generate_configs_from_base(base_cfg: dict) -> list[dict]:
     for subdir in base_actor_path.glob("*/*"):  # Assuming structure actor/class/video/
         if subdir.is_dir() and (subdir / "video.mp4").exists():
             print(f"Found video in: {subdir}")
-            
+
             new_cfg = base_cfg.clone()
             new_cfg.actor = str(subdir)  # Update actor path
+            # Save folder based on subdir
+            new_cfg.save_folder = str(base_cfg.save_folder / subdir.stem)
             new_cfg.config_name = subdir.stem  # Name based on the last folder
 
             configs.append(new_cfg)
