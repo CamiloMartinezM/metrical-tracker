551ca60c0df355197e27be2b59684eb0697375e5
Camilo Martinez <cama00005@stud.uni-saarland.de>
2025-12-27 04:48:12 +0100

    Updated

--- configure_environment.sh
+++ configure_environment.sh
@@ -14,3 +14,9 @@ unset __mamba_setup
 
 # Name of the conda environment (must match environment.yml)
 export ENV_NAME="tracker-py3.10"
+
+# CUDA configuration
+cuda_version=12.1
+export PATH=/usr/lib/cuda-${cuda_version}/bin/:${PATH}
+export LD_LIBRARY_PATH=/usr/lib/cuda-${cuda_version}/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export CUDA_PATH=/usr/lib/cuda-${cuda_version}/
